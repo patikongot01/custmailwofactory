00c50195f782bac148ca00962bdd756b9fb7c860
sornpat2 <sornpat@gmail.com>
2022-09-02 09:22:39 +0700

    done

--- src/customermailapplication/CustomerMailApplication.java
+++ src/customermailapplication/CustomerMailApplication.java
@@ -17,21 +17,37 @@ public class CustomerMailApplication {
     /**
      * @param args the command line arguments
      */
-    public static Customer getCustomerTypeUser(String customerType){
-        Customer user = null;
-        if(customerType.equals("Delinquent")){
-            user = new DelinquentCustomer();
-        }else if(customerType.equals("Mountain")){
-            user = new MountainCustomer();
-        }else if(customerType.equals("Regular")){
-            user = new RegularCustomer();
+    private Customer customer;
+    public void getCustomerTypeFromUser(String customerType) {
+        switch(customerType) {
+            case "Regular":
+                customer = new RegularCustomer();
+                break;
+            case "Mountain":
+                customer = new MountainCustomer();
+                break;
+            case "Delinquent":
+                customer = new DelinquentCustomer();
+                break;
         }
-        return user;
     }
-
+  
     public static void main(String[] args) {
-        Customer obj1 = new getCustomerTypeUser("Delinquent");
-        System.out.print(obj1);
-    
+        Customer cust = null;
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Please choose customer type 1. Regular, 2. Mountain, 3. Delinquent ");
+        int type = inp.nextInt();
+        switch(type) {
+            case 1:
+                cust = CustomerFactory.createCustomer("Regular");
+                break;
+            case 2:
+                cust = CustomerFactory.createCustomer("Mountain");
+                break;
+            case 3:
+                 cust = CustomerFactory.createCustomer("Delinquent");
+                break;
+            
+        }
     }
 }
