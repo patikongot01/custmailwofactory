352383ff0042cb937807bf079665edae858ce30c
sornpat2 <sornpat@gmail.com>
2022-09-02 08:55:01 +0700

    Done

--- src/customermailapplication/CustomerMailApplication.java
+++ src/customermailapplication/CustomerMailApplication.java
@@ -17,41 +17,21 @@ public class CustomerMailApplication {
     /**
      * @param args the command line arguments
      */
-    private Customer customer;
-    public void getCustomerTypeFromUser(String customerType) {
-        switch(customerType) {
-            case "Regular":
-                customer = new RegularCustomer();
-                break;
-            case "Mountain":
-                customer = new MountainCustomer();
-                break;
-            case "Delinquent":
-                customer = new DelinquentCustomer();
-                break;
+    public static Customer getCustomerTypeUser(String customerType){
+        Customer user = null;
+        if(customerType.equals("Delinquent")){
+            user = new DelinquentCustomer();
+        }else if(customerType.equals("Mountain")){
+            user = new MountainCustomer();
+        }else if(customerType.equals("Regular")){
+            user = new RegularCustomer();
         }
+        return user;
     }
-    public String generateMail() {
-        return customer.createMail();
-    }
-    
+
     public static void main(String[] args) {
-        CustomerMailApplication app = new CustomerMailApplication();
-        Scanner inp = new Scanner(System.in);
-        System.out.print("Please choose customer type 1. Regular, 2. Mountain, 3. Delinquent ");
-        int type = inp.nextInt();
-        switch(type) {
-            case 1:
-                app.getCustomerTypeFromUser("Regular");
-                break;
-            case 2:
-                app.getCustomerTypeFromUser("Mountain");
-                break;
-            case 3:
-                app.getCustomerTypeFromUser("Delinquent");
-                break;
-            
-        }
-        System.out.println(app.generateMail());        
+        Customer obj1 = new getCustomerTypeUser("Delinquent");
+        System.out.print(obj1);
+    
     }
 }
