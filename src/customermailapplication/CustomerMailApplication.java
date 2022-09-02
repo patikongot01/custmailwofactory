39069a1e121ef25940225e67a5b999301684ae00
sornpat2 <sornpat@gmail.com>
2022-09-02 10:02:57 +0700

    Final

--- src/customermailapplication/CustomerMailApplication.java
+++ src/customermailapplication/CustomerMailApplication.java
@@ -18,11 +18,8 @@ public class CustomerMailApplication {
      * @param args the command line arguments
      */
     private Customer customer;
-    public static Customer getCustomerTypeFromUser() {
+    public static Customer getCustomerTypeFromUser(int type) {
         Customer cust = null;
-        Scanner inp = new Scanner(System.in);
-        System.out.print("Please choose customer type 1. Regular, 2. Mountain, 3. Delinquent ");
-        int type = inp.nextInt();
         switch(type) {
             case 1:
                 cust = CustomerFactory.createCustomer("Regular");
@@ -37,8 +34,15 @@ public class CustomerMailApplication {
         return cust;
     }
   
+        public static String generateMail(Customer customer) {
+            return customer.createMail();
+    }
+          
     public static void main(String[] args) {
-        Customer obj = new getCustomerTypeFromUser();
-        System.out.println(obj.createMail());
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Please choose customer type 1. Regular, 2. Mountain, 3. Delinquent ");
+        int type = inp.nextInt();
+        Customer obj = new getCustomerTypeFromUser(type);
+        System.out.println(genrateMail(obj));
     }
 }
